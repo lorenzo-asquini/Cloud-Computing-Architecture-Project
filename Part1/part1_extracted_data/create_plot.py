5f1068589e008a9f415e87d6c3508f0206b3cdbe
LorenzoAsquini <asqlorenzo@gmail.com>
2024-03-23 22:33:55 +0100

    Improved part 1 plot

--- Part1/part1_extracted_data/create_plot.py
+++ Part1/part1_extracted_data/create_plot.py
@@ -5,6 +5,7 @@
 
 import os
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
 
 # Read data from file
 def read_data(filename):
@@ -18,21 +19,26 @@ def read_data(filename):
             data.append([float(values[0]), float(values[1]), float(values[2])])
     return data
 
+# Plot information about interference. Transform ns to ms
 def plot_file(filename, marker):
     data = read_data(filename)
     x = [float(row[0]) for row in data]
-    y = [float(row[1]) for row in data]
-    yerr_std = [row[2] for row in data]
+    y = [float(row[1]/1000) for row in data]  # Transform into ms
+    yerr_std = [row[2]/1000 for row in data]
 
     base_name = os.path.basename(filename)
     main_name = base_name.split('_')[0]
 
-    plt.errorbar(x, y, yerr=yerr_std, capsize=5, label=main_name, linewidth=2,
-                 marker=marker, markersize=8, markerfacecolor='None', markeredgewidth=2)
+    plt.errorbar(x, y, yerr=yerr_std, capsize=8, label=main_name, linewidth=2.5,
+                 marker=marker, markersize=12, markerfacecolor='None', markeredgewidth=2.5)
+
+# Express thousands with K in plot
+def thousands_formatter(x, pos):
+    return '{:.0f}K'.format(x * 1e-3)
 
 # Main names of the files
 interferences = ["none", "cpu", "l1d", "l1i", "l2", "llc", "membw"]
-markers = ['o', 's', '^', 'D', 'x', '*', 'P']
+markers = ['o', 'v', '1', 's', 'p', '*', 'X']
 
 # Plotting
 plt.figure()
@@ -40,13 +46,42 @@ for interference, marker in zip(interferences, markers):
     filename = f"{interference}_interference.txt"
     plot_file(filename, marker)
 
-plt.xlabel('avg_qps')
-plt.ylabel('avg_p95')
-plt.title('Interference Data')
-plt.legend()
-plt.grid(True)
+#### Plot look
+
+## Axis
+axis_label_font = {'fontsize': 13, 'fontweight': 'bold'}
+plt.xlabel('Queries Per Second (QPS)', fontdict=axis_label_font)
+plt.ylabel('95th percentile latency (ms)', fontdict=axis_label_font)
+
+## Title
+title_font = {'fontsize': 14, 'fontweight': 'bold'}
+plt.title('''
+95th Percentile Latency with difference interferences\n
+Error bars: Standard deviation (3 repetitions per point)
+''', fontdict=title_font, linespacing=0.5)
+
+## Ticks
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+
+formatter = FuncFormatter(thousands_formatter)
+plt.gca().xaxis.set_major_formatter(formatter)
+
+## Lines
+plt.gca().spines['top'].set_color('gray')
+plt.gca().spines['right'].set_color('gray')
+plt.gca().spines['bottom'].set_color('black')
+plt.gca().spines['left'].set_color('black')
+
+plt.grid(True, linestyle='--', linewidth=0.5, color='gray')
+
+## Legend
+handles, labels = plt.gca().get_legend_handles_labels()
+handles = [h[0] for h in handles]  # Remove error bars from legend
+plt.gca().legend(handles, labels, fontsize=13)
+
+plt.ylim(0, 8)
 
 
-plt.ylim(0, 8000)
 
 plt.show()
