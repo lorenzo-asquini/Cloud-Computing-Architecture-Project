9597a107b5b5e306761d21ea41f48cf3fa1f4ef1
LorenzoAsquini <asqlorenzo@gmail.com>
2024-04-27 23:26:06 +0200

    Add part 4.1

---
+++ Part4_1/scripts/remote_setup_memcache.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo NEEDRESTART_MODE=a apt-get update
+sudo NEEDRESTART_MODE=a apt-get install -y memcached libmemcached-tools
+
+# Necessary to expose the service now. The ip is given as argument to the script
+memcache_configuration="
+-d
+logfile /var/log/memcached.log
+-m 1024
+-p 11211
+-u memcache
+-l $1
+-P /var/run/memcached/memcached.pid
+"
+echo "$memcache_configuration" | sudo tee /etc/memcached.conf  
+sudo systemctl restart memcached
---
+++ Part4_1/scripts/setup_agent_measure.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Set project variables
+./config.sh
+
+CCA_PROJECT_PUB_KEY=${CCA_PROJECT_PUB_KEY::-4} #Remove .pub
+if [[ "$CCA_PROJECT_PUB_KEY" == *.pub ]]; then
+    echo "Path to the SSH key ends with .pub. In this case, remove it!"
+    exit 1
+fi
+
+if [ "$CCA_PROJECT_PUB_KEY" == "$DEFAULT_CCA_PROJECT_PUB_KEY" ]; then
+    echo "SSH PUB KEY value is still the placeholder one. Change it!"
+    exit 1
+fi
+
+CLIENT_AGENT_NAME=`kubectl get nodes -o custom-columns=NAME:metadata.name | grep client-agent`
+echo "#############################################"
+echo "# DEPLOYING ON $CLIENT_AGENT_NAME"
+echo "#############################################"
+gcloud compute scp --ssh-key-file $CCA_PROJECT_PUB_KEY remote_setup.sh "ubuntu@$CLIENT_AGENT_NAME:/home/ubuntu/remote_setup.sh" --zone europe-west3-a
+gcloud compute ssh --ssh-key-file $CCA_PROJECT_PUB_KEY "ubuntu@$CLIENT_AGENT_NAME" --zone europe-west3-a  -- 'cd /home/ubuntu && chmod 744 remote_setup.sh && ./remote_setup.sh'
+
+CLIENT_MEASURE_NAME=`kubectl get nodes -o custom-columns=NAME:metadata.name | grep client-measure`
+echo "#############################################"
+echo "# DEPLOYING ON $CLIENT_MEASURE_NAME"
+echo "#############################################"
+gcloud compute scp --ssh-key-file $CCA_PROJECT_PUB_KEY remote_setup.sh "ubuntu@$CLIENT_MEASURE_NAME:/home/ubuntu/remote_setup.sh" --zone europe-west3-a
+gcloud compute ssh --ssh-key-file $CCA_PROJECT_PUB_KEY "ubuntu@$CLIENT_MEASURE_NAME" --zone europe-west3-a  -- 'cd /home/ubuntu && chmod 744 remote_setup.sh && ./remote_setup.sh'
+
+MEMCACHE_SERVER_NAME=`kubectl get nodes -o custom-columns=NAME:metadata.name | grep memcache-server`
+echo "#############################################"
+echo "# RETRIEVING MEMCACHE IP ADDR"
+MEMCACHE_IPADDR=`kubectl get nodes $MEMCACHE_SERVER_NAME -o custom-columns='INTERNAL_IP:.status.addresses[?(@.type=="InternalIP")].address' | head -2 | tail -1`
+echo "# ADDR: $MEMCACHE_IPADDR"
+echo "#############################################"
+
+echo "#############################################"
+echo "# DEPLOYING ON $MEMCACHE_SERVER_NAME"
+echo "#############################################"
+gcloud compute scp --ssh-key-file $CCA_PROJECT_PUB_KEY remote_setup_memcache.sh "ubuntu@$MEMCACHE_SERVER_NAME:/home/ubuntu/remote_setup_memcache.sh" --zone europe-west3-a
+gcloud compute ssh --ssh-key-file $CCA_PROJECT_PUB_KEY "ubuntu@$MEMCACHE_SERVER_NAME" --zone europe-west3-a  -- "cd /home/ubuntu && chmod 744 remote_setup_memcache.sh && ./remote_setup_memcache.sh $MEMCACHE_IPADDR"
